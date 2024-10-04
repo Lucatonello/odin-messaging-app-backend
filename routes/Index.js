777d59b18a7f838eb10db426a602c2357be27aed
Lucatonello <lucatonellow@gmail.com>
2024-10-04 16:44:51 -0300

    new route to handle group chat creations

--- routes/Index.js
+++ routes/Index.js
@@ -186,4 +186,33 @@ router.get('/getUsers', async (req, res)  => {
     res.json(users.rows);
 });
 
+router.post('/createGroupChat/:id', verifyToken, async (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const members = req.body.members;
+    let adminUsername;
+
+    try {
+        const result = await pool.query('SELECT username FROM users WHERE id = $1', [req.params.id]);
+
+        if (result.rows.length === 0) {
+            res.status(500).json({ message: 'Error getting admin username' });
+        }
+
+        adminUsername = result.rows[0].username;
+        members.push(adminUsername);
+  
+      await pool.query(`
+          INSERT into groupchats
+          (name, description, members, admin)
+          VALUES ($1, $2, $3, $4)    
+      `, [name, description, members, adminUsername]);
+
+      res.json({ message: 'group chat created succesfully' })
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({ message: 'Error creating groupchat' });
+    }
+});
+
 module.exports = router;
