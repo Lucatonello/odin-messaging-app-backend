e4c8d854c103e78e7f47a4f4260bce9edc747ce0
Lucatonello <lucatonellow@gmail.com>
2024-10-10 15:14:24 -0300

    Add SSL configuration for PostgreSQL connection

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,9 @@
 const { Pool } = require("pg");
-require('dotenv').config(); 
+require('dotenv').config();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
